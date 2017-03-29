4b55cf61b8bdcc073410277018fe298149909de2
Othershe <shehuan320@163.com>
2017-03-29 16:32:08 +0800

    添加 拍照、相册选择图片 返回bitmap的工具类

---
+++ app/src/main/java/com/othershe/androidhelper/utils/TakeAndSelectPhotoUtil.java
@@ -0,0 +1,218 @@
+package com.othershe.androidhelper.utils;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.text.TextUtils;
+
+import com.othershe.androidhelper.BuildConfig;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 拍照、相册选择图片返回bitmap的工具类
+ */
+public class TakeAndSelectPhotoUtil {
+
+    private static File cameraFile = createFile(Environment.getExternalStorageDirectory() + "/edsfile/", "output_image.jpg");
+    private static File cacheFile = createFile(Environment.getExternalStorageDirectory() + "/edsfile/", "crop_image.jpg");
+
+    /**
+     * 启动相机
+     *
+     * @param context
+     * @param requestCode
+     */
+    public static void openCamera(Context context, int requestCode) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        Uri imageUri;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            imageUri = Uri.fromFile(cameraFile);
+        } else {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            imageUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileProvider", cameraFile);
+
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        ((Activity) context).startActivityForResult(intent, requestCode);
+    }
+
+    /**
+     * 打开相册
+     *
+     * @param context
+     * @param requestCode
+     */
+    public static void openAlbum(Context context, int requestCode) {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        ((Activity) context).startActivityForResult(intent, requestCode);
+    }
+
+    /**
+     * 剪裁图片
+     *
+     * @param context
+     * @param size        裁剪尺寸
+     * @param requestCode
+     */
+    public static void startPhotoZoom(Context context, int size, int requestCode) {
+        startPhotoZoom(context, cameraFile, size, requestCode);
+    }
+
+    /**
+     * 返回裁剪的bitmap
+     *
+     * @param context
+     * @return
+     */
+    public static Bitmap getBitmap(Context context) {
+        try {
+            return BitmapFactory.decodeStream(context.getContentResolver().openInputStream(Uri.fromFile(cacheFile)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void startPhotoZoom(Context context, File file, int size, int requestCode) {
+        try {
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            intent.setDataAndType(getImageContentUri(context, file), "image/*");//自己使用Content Uri替换File Uri
+            intent.putExtra("crop", "true");
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("outputX", size);
+            intent.putExtra("outputY", size);
+            intent.putExtra("scale", true);
+            intent.putExtra("return-data", false);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(cacheFile));//定义输出的File Uri
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+            intent.putExtra("noFaceDetection", true);
+            ((Activity) context).startActivityForResult(intent, requestCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Uri getImageContentUri(Context context, File imageFile) {
+        String filePath = imageFile.getAbsolutePath();
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[]{MediaStore.Images.Media._ID},
+                MediaStore.Images.Media.DATA + "=? ",
+                new String[]{filePath}, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor
+                    .getColumnIndex(MediaStore.MediaColumns._ID));
+            Uri baseUri = Uri.parse("content://media/external/images/media");
+            return Uri.withAppendedPath(baseUri, "" + id);
+        } else {
+            if (imageFile.exists()) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, filePath);
+                return context.getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private static File createFile(String path, String name) {
+        if (TextUtils.isEmpty(path) || TextUtils.isEmpty(name)) {
+            return null;
+        }
+
+        File parentFile = new File(path);
+        if (!parentFile.exists()) {
+            parentFile.mkdir();
+        }
+
+        File file = new File(parentFile, name);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return file;
+    }
+
+    public static void handleImage(Context context, Intent data, int size, int requestCode) {
+        if (Build.VERSION.SDK_INT >= 19) {
+            // 4.4及以上系统使用这个方法处理图片
+            handleImageOnKitKat(context, data, size, requestCode);
+        } else {
+            // 4.4以下系统使用这个方法处理图片
+            handleImageBeforeKitKat(context, data, size, requestCode);
+        }
+    }
+
+    private static void handleImageBeforeKitKat(Context context, Intent data, int size, int requestCode) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(context, uri, null);
+        startPhotoZoom(context, new File(imagePath), size, requestCode);
+    }
+
+    @TargetApi(19)
+    private static void handleImageOnKitKat(Context context, Intent data, int size, int requestCode) {
+        Uri uri = data.getData();
+        String imagePath = uriToPath(context, uri);
+        startPhotoZoom(context, new File(imagePath), size, requestCode);
+    }
+
+    private static String uriToPath(Context context, Uri uri) {
+        String path = null;
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            // 如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1]; // 解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                path = getImagePath(context, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                path = getImagePath(context, contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是content类型的Uri，则使用普通方式处理
+            path = getImagePath(context, uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是file类型的Uri，直接获取图片路径即可
+            path = uri.getPath();
+        }
+        return path;
+    }
+
+    private static String getImagePath(Context context, Uri uri, String selection) {
+        String path = null;
+        // 通过Uri和selection来获取真实的图片路径
+        Cursor cursor = context.getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+}
